ada26009cf39159d62bb0c0f477997dbfbeaa4d7
G Lokesh Kumar <lokeshkumarg4164@gmail.com>
2026-02-28 11:49:18 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,29 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
 
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import cors from "cors"
+import connect from './auth/connectDB.ts'
+import applicantRoutes from './routes/applicantRoutes.ts'
+import { config as cloudinaryConfig } from './auth/cloudinary.ts'
+
 
 const app = express();
+
+cloudinaryConfig();
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/applicant",applicantRoutes)
+const PORT = process.env.PORT
+
+connect()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    })
+
 
